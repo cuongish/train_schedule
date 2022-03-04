3992ed06aa6fca02f24d983ed04fb03fb4629f20
cuongish <hungcuong2112@gmail.com>
2022-03-05 00:18:23 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
 from library import get_train_data_from_endpoint
 from library import get_day_in_month_list
 
-days_str = get_day_in_month_list(year=2020, month=7)
-print(days_str)
+days_list = get_day_in_month_list(year=2020, month=7)
+print(days_list)
 
 train_number = 4
 
-data = get_train_data_from_endpoint(train_number=4, date=days_str[0])
-timetable_rows = data[0]['timeTableRows']
+data = [get_train_data_from_endpoint(train_number=4, date=day) for day in days_list]
+
+timetable_rows = data[0][0]['timeTableRows']
 print(data)
