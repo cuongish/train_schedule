7a7791e094690e9d546bba28bd5800b05d482033
cuongish <hungcuong2112@gmail.com>
2022-03-07 19:58:10 +0200

    Refactor vars definition

--- main.py
+++ main.py
@@ -10,21 +10,20 @@ year = 2020
 month = 7
 train_number = 4
 path = os.path.join(folder, str(year))
+meta_columns = ['trainNumber', 'departureDate', 'operatorUICCode', 'operatorShortCode',
+                'trainType', 'trainCategory', 'commuterLineID', 'runningCurrently',
+                'cancelled', 'version', 'timetableType', 'timetableAcceptanceDate']
+record_column = "timeTableRows"
 
 
 def main():
     # Get datetime.date for days in the month
     days_list = get_list_of_days_in_month(year=year, month=month)
-    csv_file_name = days_list[0].strftime('%Y-%m')
+    csv_file_name = f"{days_list[0].strftime('%Y-%m')}-train{train_number}"
 
     # Iterate API calls to retrieve train traffic data for every day in the month
     data = [get_train_data_from_endpoint(train_number=train_number, date=day)
             for day in days_list]
-
-    # Define record column and metadata columns for Normalization
-    record_column = "timeTableRows"
-    meta_columns = list(data[0][0].keys())
-    meta_columns.remove(record_column)
 
     # Normalize and output one concat df for the entire month
     result = convert_list_to_normalized_df(data=data,
